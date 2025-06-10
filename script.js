d1f89a4cd191ea2c6b3af9cbff8b627c17b1ca41
RomanRomaan <jay4or@gmail.com>
2025-06-10 16:36:45 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const uniqueID = Symbol('description');
+const bigNumber = 1234567890123456789012345678901234567890n;
+
+const obj = {};
+let a;
+console.log(typeof (123))
+console.log(typeof ('hi there!'))
+console.log(typeof (true))
+console.log(typeof (undefined))
+console.log(typeof (uniqueID))
+console.log(typeof (bigNumber))
+console.log(typeof (obj))
+console.log(null, typeof null);
